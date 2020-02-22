a5ad7d247d7e00bc6497cf8672a80d598ce58e13
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-02-22 10:05:29 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.showArrow(ArrowNames.North)
+radio.setGroup(13)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        // 1 = Forward
+        radio.sendNumber(1)
+    } else if (input.buttonIsPressed(Button.A)) {
+        // 2 = Left
+        radio.sendNumber(2)
+    } else if (input.buttonIsPressed(Button.B)) {
+        // 3 = Right
+        radio.sendNumber(3)
+    } else {
+        // 0=Stop
+        radio.sendNumber(0)
+    }
+})
